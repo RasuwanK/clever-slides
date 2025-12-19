5a50b5d072f4f2b9795d181113d5c67d4ded126e
RasuwanK <kalharaweragala@gmail.com>
2025-12-19 15:36:12 +0530

    Connected auth page link

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@
 import { TextAreaWithButton } from "@/components/ui/textarea-with-button";
 import { Logo } from "@/components/ui/logo";
 import { Typewriter } from "@/components/ui/typewriter";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <main>
-        <header className="fixed top-0 p-5">
+        <header className="flex flex-row w-full items-center fixed top-0 p-5">
           <Logo />
+          <div className="ml-auto">
+            <Link className="text-primary" href="/auth/signin">Sign in</Link>
+          </div>
         </header>
         <div id="promt-dialog" className="flex flex-col min-h-screen justify-center items-center gap-10">
           <header className="flex flex-col items-center gap-5">
