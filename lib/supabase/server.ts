3e8584b8e0ce2e8767b1cc7cb807be9582e27219
RasuwanK <kalharaweragala@gmail.com>
2025-12-19 15:38:29 +0530

    setting errror message

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -16,6 +16,7 @@ export async function createClient() {
           try {
             cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
           } catch {
+            console.error("Error while setting cookies in Server Component.");
             // The `setAll` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
             // user sessions.
