12973f5504a5b6b866a33adc54ce601324b3b258
RasuwanK <kalharaweragala@gmail.com>
2025-12-21 23:22:30 +0530

    Global auth state for pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,25 @@
-"use client";
 import { TextAreaWithButton } from "@/components/ui/textarea-with-button";
 import { Logo } from "@/components/ui/logo";
 import { Typewriter } from "@/components/ui/typewriter";
-import Link from "next/link";
+import { AuthStatus } from "@/components/ui/auth-status";
+import { createClient } from "@/lib/supabase/server";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createClient();
+
+  const {data: {user}} = await supabase.auth.getUser();
+
   return (
     <div className="flex flex-col">
       <main>
         <header className="flex flex-row w-full items-center fixed top-0 p-5">
           <Logo />
           <div className="ml-auto">
-            <Link className="text-primary" href="/auth/signin">Sign in</Link>
+            <AuthStatus isAuth={user ? true : false} user={{
+              name: user?.user_metadata?.full_name,
+              email: user?.email,
+              avatarUrl: user?.user_metadata?.avatar_url,
+            }} />
           </div>
         </header>
         <div id="promt-dialog" className="flex flex-col min-h-screen justify-center items-center gap-10">
@@ -24,6 +32,9 @@ export default function Home() {
               placeholder="Create a professional presentation for a pitch deck about an AI startup"
             />
           </form>
+        </div>
+        <div id="drafts" className="grid grid-cols-3">
+          <div className="w-10 h-10 bg-primary"></div>
         </div>
         <footer className="min-h-20 flex flex-col items-center justify-center bg-primary">
           <p className="text-white">Created by Rasuwan Kalhara</p>
