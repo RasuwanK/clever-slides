db78b594e7785b618478e099d6be14fafa7c3f1b
RasuwanK <kalharaweragala@gmail.com>
2025-12-22 14:44:28 +0530

    Removed success page

--- app/auth/success/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function AuthSuccessPage() {
-    return (
-        <>
-            <p className="text-primary text-center text-2xl font-bold">You are already logged in !</p>
-        </>
-    )
-}
