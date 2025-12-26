44134205317dc43519ecf78125ff4544ccf481fe
RasuwanK <kalharaweragala@gmail.com>
2025-12-27 01:08:20 +0530

    New types

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,10 @@
+import { SupabaseClient } from "@supabase/supabase-js";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { Database } from "./supabase/database.types";
+
+export type PresentationRow = Database["public"]["Tables"]["Presentation"]["Row"]
+export type PresentationInsert = Database["public"]["Tables"]["Presentation"]["Insert"];
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -28,4 +33,20 @@ export async function generatePresentation(payload: {
   }
 
   return responseJSON;
+}
+
+export async function createPresentation(
+  client: SupabaseClient<Database>,
+  data: PresentationInsert
+) {
+  const res = await client
+    .from("Presentation")
+    .insert([{ ...data }]).select();
+
+  // Error while creating the presentation
+  if(res.error) {
+    throw new Error("Error while creating an empty presentation");
+  }
+  
+  return res.data;
 }
