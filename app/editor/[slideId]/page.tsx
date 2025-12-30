aa1001d68adcc6cf55904ac37d494fcc063b9112
RasuwanK <kalharaweragala@gmail.com>
2025-12-30 16:00:19 +0530

    Basic presentation fetching and auto generation

--- app/editor/[slideId]/page.tsx
+++ app/editor/[slideId]/page.tsx
@@ -1,12 +1,36 @@
+import { QueryClient } from '@tanstack/react-query'
+import { createClient } from '@/lib/supabase/server'
+import { redirect } from 'next/navigation';
+import { getPresenstation } from '@/lib/utils';
+import Editor from '../editor';
+
 export default async function EditorPage({
-    params
+  params
 }: {
-    params: Promise<{slideId: string}>
+  params: Promise<{ slideId: string }>
 }) {
-    const {slideId} = await params;
-    return (
-        <div>
-            <main></main>
-        </div>
-    )
+  const supabase = await createClient();
+  const { data: { user } } = await supabase.auth.getUser();
+
+  const { slideId } = await params;
+  const queryClient = new QueryClient();
+
+  if (!user) redirect('/auth/signin')
+
+  // loading the presentation prior to page load
+  await queryClient.prefetchQuery({
+    queryKey: ["presentation"],
+    queryFn: () => getPresenstation(supabase, {
+      presentationId: slideId,
+      userId: user.id
+    })
+  });
+
+  return (
+    <div className="flex flex-col">
+      <main className="h-screen">
+        <Editor presentationId={slideId} userId={user.id} />
+      </main>
+    </div>
+  )
 }
