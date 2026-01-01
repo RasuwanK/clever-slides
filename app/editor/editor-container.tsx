e036267ba2b6a18ea0debff5fc3308e9c04eac55
Rasuwan Kalhara <kalharaweragala@gmail.com>
2026-01-02 01:30:51 +0530

    Loading the state

--- app/editor/editor-container.tsx
+++ app/editor/editor-container.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { EditorStateGate } from "@/components/editor-state-gate";
-import { useGeneratePresentation } from "@/hooks/use-generate-presentation";
 import { usePresentation } from "@/hooks/use-presentation";
 import { createClient } from "@/lib/supabase/client"
+import { Content } from "@/lib/utils";
+import { useEditorStore } from "@/stores/editor-store";
 import { useRouter } from "next/navigation";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 interface EditorProps {
   presentationId: string;
@@ -17,34 +17,63 @@ export default function EditorContainer({ presentationId, userId }: EditorProps)
   const router = useRouter();
 
   // Fetching and storing the presentation
-  const {data: presentation, isLoading, error} = usePresentation({
+  const { data: presentation, isLoading, error } = usePresentation({
     supabase,
     presentationId,
     userId
   });
 
-  const generateMutation = useGeneratePresentation();
+  const setSlides = useEditorStore((s) => s.setSlides);
+  const slides = useEditorStore((s) => s.slides);
+
+  console.log(presentation)
+
+  // As soon as the data is changed presentation is loaded
+  useEffect(() => {
+    if (presentation?.content) {
+      // To make the typecase convenient it is stored as a string
+      setSlides(JSON.parse(presentation.content.toString()) as Content);
+    } else {
+      setSlides({
+        theme: {
+          accentColor: "white",
+          background: "light"
+        },
+        slides: [
+          {
+            layout: "title_center",
+            title: "Sample title",
+            bullets: [
+              "Sample bullet",
+            ]
+          }
+        ]
+      })
+    }
+  }, [presentation?.id])
 
   useEffect(() => {
-    if(error?.message === "NOT_FOUND") {
+    if (error?.message === "NOT_FOUND") {
       router.push("/404");
     }
   }, [error]);
 
-  if(isLoading) return <p>Loading</p>
+  if (isLoading) return <p>Loading</p>
 
-  if(!presentation) return null;
+  if (!presentation) return null;
+
+  // TODO: HANDLE when presentation has not generated
+  if (!presentation.content) return null
 
   return (
-    <EditorStateGate presentation={presentation} onGenerate={(prompt) => {
-      // generateMutation.mutate({prompt}, {
-      //   onSuccess: (schema) => {
-      //     console.log(schema);
-      //   },
-      //   onError: () => {
-      //     console.log("An error has occured");
-      //   }
-      // });
-    }} />
+    <div id="editor-page-container" className="flex flex-col min-h-screen">
+      <main id="editor-page" className="flex flex-col">
+        <div id="editor" className="grid grid-cols-2">
+          <div id="slides-nav">
+            {JSON.stringify(slides)}
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
