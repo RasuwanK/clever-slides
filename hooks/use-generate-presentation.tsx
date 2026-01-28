6782538143232a0fbc337d929fe6c1e0a7c3f12a
Rasuwan Kalhara <49600817+RasuwanK@users.noreply.github.com>
2026-01-28 14:32:41 +0530

    Added function save on generated

--- hooks/use-generate-presentation.tsx
+++ hooks/use-generate-presentation.tsx
@@ -1,10 +1,22 @@
 "use client";
 
-import { useMutation } from "@tanstack/react-query"
-import { generatePresentation } from "@/lib/utils/supabase"
+import { useMutation } from "@tanstack/react-query";
+import { GeneratedContent, generatePresentation } from "@/lib/utils/supabase";
 
-export function useGeneratePresentation() {
-    return useMutation({
-        mutationFn: generatePresentation
-    });
+interface UseGeneratePresentationProps {
+  saveFn: (data: GeneratedContent) => void;
+}
+
+export function useGeneratePresentation({
+  saveFn,
+}: UseGeneratePresentationProps) {
+  return useMutation({
+    mutationFn: generatePresentation,
+    onSuccess: (data) => {
+      saveFn(data);
+    },
+    onError: () => {
+      console.error("Error while generating presentation");
+    },
+  });
 }
