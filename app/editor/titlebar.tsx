0e9062c27f832bc0d99766f45eeecb044fdd1210
Rasuwan Kalhara <49600817+RasuwanK@users.noreply.github.com>
2026-02-01 13:27:21 +0530

    Included major elements for editor titlebar

--- app/editor/titlebar.tsx
+++ app/editor/titlebar.tsx
@@ -1,14 +1,27 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { AuthStatus } from "@/components/ui/dynamic/auth-status";
 import { DownloadSimpleIcon, PresentationIcon } from "@phosphor-icons/react";
+import { User } from "@/components/ui/auth-status";
+import { ArrowLeftIcon } from "@phosphor-icons/react";
+import Link from "next/link";
 
-export function Titlebar() {
+interface TitlebarProps {
+  user?: User;
+  title: string;
+}
+
+export function Titlebar({ user, title }: TitlebarProps) {
   return (
     <nav id="navbar" className="w-full flex flex-row gap-2 items-center">
+      <Link href="/">
+        <ArrowLeftIcon size={20} className="text-primary mr-4" weight="bold" />
+      </Link>
       <div id="file-info" className="flex flex-col">
-        <h1 className="text-xl font-bold">
-          {/* {presentation?.content.title} */}
+        <h1 className="text-xl">
+          {title}
         </h1>
+        <p className="text-sm">Last updated 2025/02/02</p>
         {/* <h2 className="text-sm">{presentation?.updated_at ?? ""}</h2> */}
       </div>
       <div
@@ -24,6 +37,12 @@ export function Titlebar() {
             <PresentationIcon size={32} />
             Present
           </Button>
+        </div>
+        <div id="auth-status">
+          <AuthStatus
+            isAuth={user ? true : false}
+            user={user}
+          />
         </div>
       </div>
     </nav>
