2ecbcab2e3d724f9e0d3bbb01cc01006f732e8eb
Rasuwan Kalhara <49600817+RasuwanK@users.noreply.github.com>
2026-02-06 02:12:05 +0530

    changed to string keys

--- lib/types/canvas.ts
+++ lib/types/canvas.ts
@@ -1,49 +1,49 @@
 export type Document = {
-  title: string;
-  theme: {
-    backgroundColor: string;
-    foregroundColor: string;
+  "title": string;
+  "theme": {
+    "backgroundColor": string;
+    "foregroundColor": string;
   };
-  resolution: {
-    width: number; // Resolution in pixels
-    height: number;
+  "resolution": {
+    "width": number; // Resolution in pixels
+    "height": number;
   };
-  version: number;
-  createdAt: string;
-  updatedAt: string;
-  slides: {
-    slideNumber: number;
-    comments: {
-      position: {
-        x: number;
-        y: number;
+  "version": number;
+  "createdAt": string;
+  "updatedAt": string;
+  "slides": {
+    "slideNumber": number;
+    "comments": {
+      "position": {
+        "x": number;
+        "y": number;
       };
-      message: string;
-      isResolved: boolean;
+      "message": string;
+      "isResolved": boolean;
     }[];
-    content: RenderTree;
+    "content": RenderTree;
   }[];
 };
 
 // Refer to a single node in the render tree
 export type Element = {
-    id: string;
-    name: string;
-    cords: {
-        x: number;
-        y: number;
+    "id": string;
+    "name": string;
+    "cords": {
+        "x": number;
+        "y": number;
     };
-    dimensions: {
-        width: number;
-        height: number;
+    "dimensions": {
+        "width": number;
+        "height": number;
     };
-    padding: number;
-    backgroundColor: string;
-    foregroundColor: string;
-    children?: Element[];
+    "padding": number;
+    "backgroundColor": string;
+    "foregroundColor": string;
+    "children"?: Element[];
 }
 
 // Refer to the data structure which contains all the rendering elements in order
 export type RenderTree = {
-    root: Element;
+    "root": Element;
 }
